bd2f7383588b49473b61a9b6f297fceb916eb84d
Gadnyz <38428063+gadnyz@users.noreply.github.com>
2026-04-16 13:34:52 +0200

    refactor: apply base frontend architecture and update routing

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -3,7 +3,7 @@ import { AppLayout } from './app/layout/component/app.layout';
 import { Dashboard } from './app/pages/dashboard/dashboard';
 import { Documentation } from './app/pages/documentation/documentation';
 import { Landing } from './app/pages/landing/landing';
-import { Notfound } from './app/pages/notfound/notfound';
+import { Notfound } from './app/core/navigation/notfound';
 
 export const appRoutes: Routes = [
     {
@@ -18,6 +18,6 @@ export const appRoutes: Routes = [
     },
     { path: 'landing', component: Landing },
     { path: 'notfound', component: Notfound },
-    { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
+    { path: 'auth', loadChildren: () => import('./app/core/auth/auth.routes') },
     { path: '**', redirectTo: '/notfound' }
 ];
